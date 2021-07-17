876d0eb38f429d3c3bffb5e4d52b0687a7b1afc4
Troy Dorman <troydorman@gmail.com>
2021-07-16 20:27:08 -0600

    changed our main-headers background color variable thru javascri[t and applied the blue color to it

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 const box = document.querySelector(".box");
 const boxStyles = getComputedStyle(box);
+const boxMainColor = boxStyles.getPropertyValue("--box-main-color");
+const header = document.querySelector("#main-header");
+header.style.setProperty("--header-bg-color", boxMainColor);
